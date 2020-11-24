d97fe5663b694642adb6244c7d6e26893ba74ced
Seongkyun-Yu <ysungkyun@gmail.com>
2020-11-25 00:49:53 +0900

    feat: Make LoginForm

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,9 +1,14 @@
+/* eslint-disable react/react-in-jsx-scope */
 import Link from "next/link";
-
 import PropTypes from "prop-types";
 import { Menu, Input, Row, Col } from "antd";
 
+import UserProfile from "./UserProfile";
+import LoginForm from "./LoginForm";
+import { useState } from "react";
+
 const AppLayout = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div>
       <Menu mode="horizontal">
@@ -28,7 +33,7 @@ const AppLayout = ({ children }) => {
       </Menu>
       <Row gutter={8}>
         <Col xs={24} md={6}>
-          왼쪽메뉴{" "}
+          {isLoggedIn ? <UserProfile /> : <LoginForm />}
         </Col>
         <Col xs={24} md={12}>
           {children}{" "}
