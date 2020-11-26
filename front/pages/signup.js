b9f91938abb257773175ab8cf4799c569a6e81d6
Seongkyun-Yu <ysungkyun@gmail.com>
2020-11-26 21:11:43 +0900

    feat: Make useInput custom hook

--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useCallback } from "react";
 import Head from "next/head";
+import { Form, Input } from "antd";
 import AppLayout from "../components/AppLayout";
 
 const Signup = () => {
+  const onSubmit = useCallback(() => {});
   return (
-    <>
+    <AppLayout>
       <Head>
         <title>NodeBird | 회원가입</title>
       </Head>
-      <AppLayout>
-        <div>회원가입 페이지</div>
-      </AppLayout>
-    </>
+
+      <Form onFinish={onSubmit}>
+        <div>
+          <label htmlFor="user-id">아이디</label>
+          <br />
+          <Input name="user-id" value={id} onChange={onChangeId} required />
+        </div>
+      </Form>
+    </AppLayout>
   );
 };
 
