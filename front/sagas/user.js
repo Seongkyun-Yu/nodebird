1ba5bc0172dd0a6d0532ce21ce640077e62e171a
Seongkyun-Yu <ysungkyun@gmail.com>
2020-12-08 23:35:19 +0900

    feat: Apply logOut api

--- front/sagas/user.js
+++ front/sagas/user.js
@@ -46,8 +46,8 @@ function logOutAPI() {
 
 function* logOut() {
   try {
-    // const result = yield call(logOutAPI);
-    yield delay(1000);
+    const result = yield call(logOutAPI);
+
     yield put({
       type: LOG_OUT_SUCCESS,
     });
