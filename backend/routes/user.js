43dbb4b3b3da61505dc2cf54ce326cee4f899e82
Seongkyun-Yu <ysungkyun@gmail.com>
2020-12-16 22:36:30 +0900

    feat: Make following & unfollowing api

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -121,6 +121,7 @@ router.post("/logout", isLoggedIn, (req, res, next) => {
 });
 
 router.patch("/nickname", isLoggedIn, async (req, res, next) => {
+  // PATCH /user/1/follow
   try {
     await User.update(
       {
@@ -131,6 +132,36 @@ router.patch("/nickname", isLoggedIn, async (req, res, next) => {
       }
     );
     res.status(200).json({ nickname: req.body.nickname });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.patch("/:userId/follow", isLoggedIn, async (req, res, next) => {
+  // PATCH /user/1/follow
+  try {
+    const user = await User.findOne({ where: { id: req.params.userId } });
+    if (!user) {
+      res.status(403).send("없는 사람을 팔로우하려고 하시네요");
+    }
+    await user.addFollowers(req.user.id);
+    res.status(200).json({ UserId: parseInt(req.params.userId) });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.delete("/:userId/follow", isLoggedIn, async (req, res, next) => {
+  // DELETE /user/1/follow
+  try {
+    const user = await User.findOne({ where: { id: req.params.userId } });
+    if (!user) {
+      res.status(403).send("없는 사람을 언팔로우하려고 하시네요");
+    }
+    await user.removeFollowers(req.user.id);
+    res.status(200).json({ UserId: parseInt(req.params.userId) });
   } catch (error) {
     console.error(error);
     next(error);
